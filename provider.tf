fa55aeead2f0f477eccf7042ddec1839a8efa239
Your-me <39601920+Your-me@users.noreply.github.com>
2023-03-05 15:42:16 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
     }
   }
 }
 provider "aws" {
   region = var.region
+  access_key = var.access-key
+  secret_key = var.secret-key
 }
