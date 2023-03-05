12a5a424c07a7730610df374ab98d536601907fe
Your-me <39601920+Your-me@users.noreply.github.com>
2023-03-05 17:32:22 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -56,7 +56,7 @@ variable "igw-cidr-block" {
 variable "ami_name" {
      type = string
      description = "server configuration"
-     default = "ami-0f540e9f488cfa27d"
+     default = "ami-0a145236ee857b126"
 
 }     
 
